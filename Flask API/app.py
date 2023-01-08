355dd00ea06fe38c208d14060e7c91f0bb0925e1
Harry Chauhan <33790468+harrycha@users.noreply.github.com>
2023-01-07 22:34:47 -0700

    Changed files for cloud run
    Modified api.py to app.py, new docker and requirements and Procfile

---
+++ Flask API/app.py
@@ -0,0 +1,36 @@
+"""
+A sample Hello World server. With a reponame API call
+"""
+import os
+
+from flask import Flask, render_template
+
+# pylint: disable=C0103
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    message = "It's running!"
+
+    """Get Cloud Run environment variables."""
+    service = os.environ.get('K_SERVICE', 'Unknown service')
+    revision = os.environ.get('K_REVISION', 'Unknown revision')
+
+    return render_template('index.html',
+        message=message,
+        Service=service,
+        Revision=revision)
+
+@app.route('/reponame', methods = ['GET'])
+def return_data():
+    input_repo = str(request.args(['reponame']))
+    # Api call
+    
+    # Data format
+    return output
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
