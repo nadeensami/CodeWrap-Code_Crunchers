29edff677d69879628bd12a4235ecc37fd9b78df
Harry Chauhan <33790468+harrycha@users.noreply.github.com>
2023-01-08 09:35:57 -0700

    Last update
    Issues with authentication on /api2/ route

--- Flask API/app.py
+++ Flask API/app.py
@@ -1,10 +1,107 @@
 """
 A sample Hello World server. With a reponame API call
 """
-import os, base64, requests, re, ast
+import os, base64, requests, re, ast, json
 
 from flask import Flask, render_template
-# import openai
+
+# pylint: disable=C0103
+app = Flask(__name__)
+
+# Create global metrics
+numLines = 0
+numComments = 0
+variableDict = {}
+numForLoops = 0
+maxIndentDepth = 0
+varTypeCounts = {'flatCase':0, 'camelCase': 0, 'snakeCase': 0, 'kebabCase': 0}
+
+# Set the request headers
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": "Bearer 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",
+    "X-GitHub-Api-Version": "2022-11-28"
+}
+
+
+@app.route('/')
+def hello():
+    """Return a friendly HTTP greeting."""
+    return 'hey'
+
+@app.route('/api/<owner>')
+def api(owner):
+    repos = getRepos(owner)
+    total_repo_commits = []
+    for repo in repos:
+        url_stats = "https://api.github.com/repos/{}/{}/stats/punch_card".format(owner, repo)
+        # Make the request
+        response = requests.get(url_stats, headers=headers)
+        byte_array_str = response.content.decode()
+        # Use ast.literal_eval to parse the string and return the corresponding value
+        normal_list = ast.literal_eval(byte_array_str)
+        if isinstance(normal_list, list):
+            for commits_list in normal_list:
+                total_repo_commits.append(commits_list)
+    
+    time_of_day = determine_time_of_day(total_repo_commits)
+    # Function to determine if someone is a night owl or early bird
+
+    # Create the JSON object
+    json_obj = {"paragraph": "", "code_comment": "", "bird": ""}
+    
+    # Fill in the values
+    json_obj["code_comment"] = "blaber mouth"
+    json_obj["bird"] = "early bird"
+
+    # Convert the JSON object to a string and print it
+    json_string = json.dumps(json_obj)
+
+    # Data format
+    output = """{
+"paragraph": "A variable named 'animal' holds a creature in its grasp\nIts value unknown, until we look upon its face\nFrom Python's shell, we call it into view\nA tiger, a bear, or maybe a flamingo too\nBut whether it roars or slinks with grace\nWe'll treat it kindly and give it its place",
+"code_comment": "blaber mouth",
+"bird": "early bird"
+}"""
+    return output
+    
+
+    
+@app.route('/api2/<owner>')
+def api2(owner):
+    # Api call
+    print(owner)
+    # repos = getRepos(owner)
+    # hard coded public repos
+    repos = ["hacked2023", "complit", "nadeensami.github.io", "aqa-test-tools", "html-css-ws", "openj9", "adas-team-website", "CS-books", "aqa-tests", "TKG", "spacestagram"]
+    for repo in repos:
+        parseFiles(owner, repo)
+
+    # Tokenize the variable
+    classifyVariables()
+
+    # Get maximum words used and send to chat GPT
+    mostCommonVars = sorted(variableDict, variableDict.get)[-6:]
+    
+    # Set the URL for the request
+    total_repo_commits = []
+    for repo in repos:
+        url_stats = "https://api.github.com/repos/{}/{}/stats/punch_card".format(owner, repo)
+        # Make the request
+        response = requests.get(url_stats, headers=headers)
+        byte_array_str = response.content.decode()
+        # Use ast.literal_eval to parse the string and return the corresponding value
+        normal_list = ast.literal_eval(byte_array_str)
+        if isinstance(normal_list, list):
+            for commits_list in normal_list:
+                total_repo_commits.append(commits_list)
+    
+    time_of_day = determine_time_of_day(total_repo_commits)
+    # Function to determine if someone is a night owl or early bird
+
+    # Data format
+    
+    return time_of_day
 
 def determine_time_of_day(commits):
     morning_commits = 0
@@ -25,29 +122,6 @@ def determine_time_of_day(commits):
         return "morning"
     else:
         return "evening"
-
-def getRepos(username):
-    page = 1
-    url = f'https://api.github.com/users/{username}/repos?type=all&per_page=10&page={page}'
-    r = requests.get(url)
-    r.raise_for_status()
-    data = r.json()
-
-    repos = [repoObject['name'] for repoObject in data]
-    while len(data) > 0:
-        page += 1
-        url = f'https://api.github.com/users/{username}/repos?type=all&per_page=10&page={page}'
-        r = requests.get(url)
-        r.raise_for_status()
-        data = r.json()
-        repos += [repoObject['name'] for repoObject in data]
-    print(repos)
-    return repos
-
-def generate_poem():
-    openai.api_key = os.getenv("OPENAI_API_KEY")
-    return poem
-
 
 # num for loops --> regex
 def getNumFor(fileString):
@@ -101,7 +175,7 @@ def getNumComments(fileString):
 
 def readFile(fullName, filePath):
   url = f'https://api.github.com/repos/{fullName}/contents/{filePath}'
-  r = requests.get(url)
+  r = requests.get(url, headers=headers)
   r.raise_for_status()
   data = r.json()
   file_content = data['content']
@@ -111,32 +185,31 @@ def readFile(fullName, filePath):
 
   return file_content
 
-def getRepos(username):
-  page = 1
-  url = f'https://api.github.com/users/{username}/repos?type=all&per_page=10&page={page}'
-  r = requests.get(url)
-  r.raise_for_status()
-  data = r.json()
+# def getRepos(username):
+#   page = 1
+#   url = f'https://api.github.com/users/{username}/repos?type=all&per_page=10&page={page}'
+#   r = requests.get(url, headers=headers)
+#   r.raise_for_status()
+#   data = r.json()
 
-  repos = data
-  while len(data) > 0:
-    page += 1
-    url = f'https://api.github.com/users/{username}/repos?type=all&per_page=10&page={page}'
-    r = requests.get(url)
-    r.raise_for_status()
-    data = r.json()
-    repos += data
+#   repos = data
+#   while len(data) > 0:
+#     page += 1
+#     url = f'https://api.github.com/users/{username}/repos?type=all&per_page=10&page={page}'
+#     r = requests.get(url, headers=headers)
+#     r.raise_for_status()
+#     data = r.json()
+#     repos += data
 
-  return repos
+#   return repos
 
-def parseFiles(repoObject):
+def parseFiles(username, repo):
   dirQueue = ['']
 
   while len(dirQueue) > 0:
     currDir = dirQueue.pop(0)
-
-    url = f'https://api.github.com/repos/{repoObject["full_name"]}/contents/{currDir}'
-    r = requests.get(url)
+    url = f'https://api.github.com/repos/{username}/{repo}/contents/{currDir}'
+    r = requests.get(url, headers=headers)
     r.raise_for_status()
     data = r.json()
 
@@ -145,7 +218,7 @@ def parseFiles(repoObject):
         dirQueue.append(file['path'])
       elif file['name'][-3:] == '.py':
         # Get file string
-        fileString = readFile(repoObject["full_name"], file['path'])
+        fileString = readFile(f"{username}/{repo}", file['path'])
 
         # Get number of for loops
         numForLoops += getNumFor(fileString)
@@ -163,62 +236,6 @@ def parseFiles(repoObject):
 
         # Get number of lines of comments
         numComments += getNumComments(fileString)
-
-# Set the request headers
-headers = {
-    "Accept": "application/vnd.github+json",
-    "Authorization": "Bearer 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"
-}
-
-# pylint: disable=C0103
-app = Flask(__name__)
-
-# Create global metrics
-numLines = 0
-numComments = 0
-variableDict = {}
-numForLoops = 0
-maxIndentDepth = 0
-varTypeCounts = {'flatCase':0, 'camelCase': 0, 'snakeCase': 0, 'kebabCase': 0}
-
-@app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'hey'
-
-@app.route('/api/<owner>')
-def api(owner):
-    # Api call
-    print(owner)
-    repos = getRepos(owner)
-    for repo in repos:
-        parseFiles(owner, repo)
-
-    # Tokenize the variable
-    classifyVariables()
-
-    # Get maximum words used and send to chat GPT
-    mostCommonVars = sorted(variableDict, variableDict.get)[-6:]
-
-    # Set the URL for the request
-    total_repo_commits = []
-    for repo in repos:
-        url_stats = "https://api.github.com/repos/{}/{}/stats/punch_card".format(owner, repo)
-        # Make the request
-        response = requests.get(url_stats, headers=headers)
-        byte_array_str = response.content.decode()
-        # Use ast.literal_eval to parse the string and return the corresponding value
-        normal_list = ast.literal_eval(byte_array_str)
-        if isinstance(normal_list, list):
-            for commits_list in normal_list:
-                total_repo_commits.append(commits_list)
-    
-    time_of_day = determine_time_of_day(total_repo_commits)
-    # Function to determine if someone is a night owl or early bird
-
-    # Data format
-    
-    return time_of_day
 
 if __name__ == '__main__':
     server_port = os.environ.get('PORT', '8080')
